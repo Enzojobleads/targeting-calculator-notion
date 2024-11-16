6794a29b5b9e3f0e804c4e0bb438a6ae1bdc4f72
Enzojobleads <enzo.luciano@job-leads.fr>
2024-11-16 15:01:51 +0800

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { TargetingCalculator } from '../components/targeting/TargetingCalculator'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-gray-50 p-4">
+      <TargetingCalculator />
+    </div>
+  )
+}
